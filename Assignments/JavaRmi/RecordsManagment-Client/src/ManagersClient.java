2e9adb72c884d24cae28fa4dfdc78d4e4f53ad59
Chris McArthur <prince.chrismc@gmail.com>
2018-10-06 09:49:17 -0400

    correcting license

--- Assignments/JavaRmi/RecordsManagment-Client/src/ManagersClient.java
+++ Assignments/JavaRmi/RecordsManagment-Client/src/ManagersClient.java
@@ -1,8 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* 
+    MIT License
+
+    Copyright (c) 2018 Chris Mc, prince.chrismc(at)gmail(dot)com
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
+import Models.Region;
+import Server.RegionalRecordManager;
+import java.rmi.Naming;
 
 /**
  *
@@ -13,8 +35,13 @@ public class ManagersClient {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException {
         // TODO code application logic here
+
+        RegionalRecordManager look_up = (RegionalRecordManager) Naming.lookup("rmi://localhost/" + Region.CA.toString());
+        String txt = JOptionPane.showInputDialog("What is your name?");
+
+        String response = look_up.helloTo(txt);
+        JOptionPane.showMessageDialog(null, response);
     }
-    
 }
