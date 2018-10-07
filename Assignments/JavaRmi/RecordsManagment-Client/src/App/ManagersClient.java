8b4af70b80351e44ef4bdcc3672c7462a6bf9d50
Chris McArthur <prince.chrismc@gmail.com>
2018-10-07 16:43:30 -0400

    outline for client application
    missing implementation for operations

--- Assignments/JavaRmi/RecordsManagment-Client/src/App/ManagersClient.java
+++ Assignments/JavaRmi/RecordsManagment-Client/src/App/ManagersClient.java
@@ -24,10 +24,8 @@
 package App;
 
 import Client.RegionalClient;
-import Models.Feild;
-import Models.Project;
-import Models.ProjectIdentifier;
 import Models.Region;
+import java.util.Scanner;
 
 /**
  *
@@ -39,14 +37,93 @@ public class ManagersClient {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Welcome to Christopher McArthur's Distributed Employee Management System\r\n");
 
-        // Welcome to Christopher McArthur's Distributed Employee Management System
-        // DEMS Login...
-        // Human Resource Manager ID:
-        ///// CA1111
-        // ID.size == 6 ~~> Try agains!
-        // switch ( ID.prefix ) --> new RegionalClient for that region
-        //                      ~~> try again!
-        // Perform Operation { Create... Edit... Get... Exit} While ! Exit
+        System.out.println("DEMS Login...");
+
+        boolean loginIsValid = true;
+        String userLogin;
+        Scanner reader = new Scanner(System.in);
+        
+        do {
+            System.out.println("Please enter your 'Human Resources Manager ID': ");
+            userLogin = reader.nextLine();
+
+            if (userLogin.length() != 6) {
+                System.out.println("Invalid ID length. Format region code + 4 digits. Ex: 'CA1111'");
+                loginIsValid = false;
+            }
+
+            try {
+                Region.fromString(userLogin.substring(0, 2));
+            } catch (Exception ex) {
+                System.out.println("Invalid ID region code. Available { CA, US, UK }. Ex: 'CA1111'");
+                loginIsValid = false;
+            }
+
+        } while (!loginIsValid);
+        
+        RegionalClient client = null;
+        try {
+             client = new RegionalClient(userLogin);
+        } catch (Exception ex) {
+            System.out.println("Unable to establish connection! Please try again later...");
+            System.exit(-1);
+        }
+        
+        
+        try {
+            boolean userRequestedExit = false;
+            do{
+                System.out.println("What operation would you like to perform?");
+                System.out.println("   1. Create manager record");
+                System.out.println("   2. Create employee record");
+                System.out.println("   3. Edit an existing record");
+                System.out.println("   4. Display total number of records");
+                System.out.println("   5. Exit");
+                System.out.println("Selection: ");
+                
+                int userSelection = reader.nextInt();
+                
+                switch( userSelection ){
+                    case 1:
+                        createManagerRecord(client);
+                        break;
+                    case 2:
+                        createEmployeeRecord(client);
+                        break;
+                    case 3:
+                        editExistingRecord(client);
+                        break;
+                    case 4:
+                        System.out.println( client.getRecordCount() );
+                        break;
+                    case 5:
+                        System.out.println( "Good-bye!" );
+                        userRequestedExit = true;
+                        break;
+                    default:
+                        System.out.println( "Invalid selection" );
+                        break;
+                }
+
+            }while(!userRequestedExit);
+            
+        } catch (Exception ex) {
+            System.out.println("Unable to perform requested operation! System failure! Please try again later...");
+            System.exit(-1);
+        }
+    }
+
+    private static void createManagerRecord(RegionalClient client) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void createEmployeeRecord(RegionalClient client) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void editExistingRecord(RegionalClient client) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
