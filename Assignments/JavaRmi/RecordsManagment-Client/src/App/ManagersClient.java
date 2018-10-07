0acd6066967236b2dc58febbc35122d112e3c448
Chris McArthur <prince.chrismc@gmail.com>
2018-10-07 18:08:00 -0400

    complete client application

--- Assignments/JavaRmi/RecordsManagment-Client/src/App/ManagersClient.java
+++ Assignments/JavaRmi/RecordsManagment-Client/src/App/ManagersClient.java
@@ -24,7 +24,11 @@
 package App;
 
 import Client.RegionalClient;
+import Models.Project;
+import Models.ProjectIdentifier;
 import Models.Region;
+import Models.Feild;
+import java.rmi.RemoteException;
 import java.util.Scanner;
 
 /**
@@ -44,9 +48,9 @@ public class ManagersClient {
         boolean loginIsValid = true;
         String userLogin;
         Scanner reader = new Scanner(System.in);
-        
+
         do {
-            System.out.println("Please enter your 'Human Resources Manager ID': ");
+            System.out.print("Please enter your 'Human Resources Manager ID': ");
             userLogin = reader.nextLine();
 
             if (userLogin.length() != 6) {
@@ -62,30 +66,29 @@ public class ManagersClient {
             }
 
         } while (!loginIsValid);
-        
+
         RegionalClient client = null;
         try {
-             client = new RegionalClient(userLogin);
+            client = new RegionalClient(userLogin);
         } catch (Exception ex) {
             System.out.println("Unable to establish connection! Please try again later...");
             System.exit(-1);
         }
-        
-        
+
         try {
             boolean userRequestedExit = false;
-            do{
+            do {
                 System.out.println("What operation would you like to perform?");
                 System.out.println("   1. Create manager record");
                 System.out.println("   2. Create employee record");
                 System.out.println("   3. Edit an existing record");
                 System.out.println("   4. Display total number of records");
                 System.out.println("   5. Exit");
-                System.out.println("Selection: ");
-                
+                System.out.print("Selection: ");
+
                 int userSelection = reader.nextInt();
-                
-                switch( userSelection ){
+
+                switch (userSelection) {
                     case 1:
                         createManagerRecord(client);
                         break;
@@ -96,34 +99,116 @@ public class ManagersClient {
                         editExistingRecord(client);
                         break;
                     case 4:
-                        System.out.println( client.getRecordCount() );
+                        System.out.println(client.getRecordCount());
                         break;
                     case 5:
-                        System.out.println( "Good-bye!" );
+                        System.out.println("Good-bye!");
                         userRequestedExit = true;
                         break;
                     default:
-                        System.out.println( "Invalid selection" );
+                        System.out.println("Invalid selection");
                         break;
                 }
 
-            }while(!userRequestedExit);
-            
+            } while (!userRequestedExit);
+
         } catch (Exception ex) {
             System.out.println("Unable to perform requested operation! System failure! Please try again later...");
             System.exit(-1);
         }
     }
 
-    private static void createManagerRecord(RegionalClient client) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private static void createManagerRecord(RegionalClient client) throws RemoteException, Exception {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("To create a new manager record certain information is required...");
+
+        System.out.println("First Name:");
+        String firstName = reader.nextLine();
+
+        System.out.println("Last Name:");
+        String lastName = reader.nextLine();
+
+        System.out.println("Employee ID:");
+        int employeeId = reader.nextInt();
+        reader.nextLine();
+        
+        System.out.println("Email:");
+        String mailId = reader.nextLine();
+
+        System.out.println("Project ID: (5 digits)");
+        int projectId = reader.nextInt();
+        reader.nextLine();
+
+        System.out.println("Project Name:");
+        String projectName = reader.nextLine();
+
+        System.out.println("Project Client:");
+        String projectClient = reader.nextLine();
+
+        System.out.println("Location:");
+        String location = reader.nextLine();
+
+        String newRecordId = client.createMRecord(firstName, lastName, employeeId, mailId, new Project(
+                new ProjectIdentifier(projectId), projectName, projectClient), location);
+
+        System.out.println("Successfully created record: " + newRecordId);
+
     }
 
-    private static void createEmployeeRecord(RegionalClient client) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private static void createEmployeeRecord(RegionalClient client) throws RemoteException {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("To create a new employee record certain information is required...");
+
+        System.out.println("First Name:");
+        String firstName = reader.nextLine();
+
+        System.out.println("Last Name:");
+        String lastName = reader.nextLine();
+
+        System.out.println("Employee ID:");
+        int employeeId = reader.nextInt();
+        reader.nextLine();
+
+        System.out.println("Email:");
+        String mailId = reader.nextLine();
+
+        System.out.println("Project ID: (P#####) ");
+        String projectId = reader.nextLine();
+
+        String newRecordId = client.createERecord(firstName, lastName, employeeId, mailId, projectId);
+
+        System.out.println("Successfully created record: " + newRecordId);
+
     }
 
-    private static void editExistingRecord(RegionalClient client) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private static void editExistingRecord(RegionalClient client) throws RemoteException {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("To create a new employee record certain information is required...");
+        System.out.print("   Possible feild values are { ");
+        for (Feild feild : Feild.values()) {
+            System.out.print(feild.toString() + ", ");
+        }
+        System.out.println(" }");
+
+        System.out.println("Record ID:");
+        String recordId = reader.nextLine();
+
+        System.out.println("Feild Name:");
+        String feildName = reader.nextLine();
+
+        Object newValue;
+        if (feildName.equals("employee id")) {
+            newValue = reader.nextInt();
+            reader.nextLine();
+        } else {
+            newValue = reader.nextLine();
+        }
+
+        String newRecordId = client.editRecord(recordId, feildName, newValue);
+
+        System.out.println("Successfully edited record: " + newRecordId);
     }
 }
