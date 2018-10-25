765aa5008a3bbacd7fe65f618f47f13df8b5537e
Chris McArthur <prince.chrismc@gmail.com>
2018-10-25 16:36:21 -0400

    correcting client to work with correctly names server

---
+++ Assignments/Corba/RecMgnt-CorbaClient/src/App/ManagersClient.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package App;
+
+import Interface.Corba.DEMS.RegionalRecordManipulator;
+import Interface.Corba.DEMS.RegionalRecordManipulatorHelper;
+import Models.Region;
+import java.util.Scanner;
+import org.omg.CORBA.ORB;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+
+/**
+ *
+ * @author cmcarthur
+ */
+public class ManagersClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        try {
+            ORB orb = ORB.init(args, null);
+            org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
+            NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+            RegionalRecordManipulator addobj = (RegionalRecordManipulator) RegionalRecordManipulatorHelper.narrow(ncRef.resolve_str(Region.CA.toString()));
+
+            Scanner c = new Scanner(System.in);
+            System.out.println("Welcome to the addition system:");
+            System.out.println("The result for addition is : " + addobj.getRecordCount());
+            System.out.println("-----------------------------------");
+        } catch (Exception e) {
+            System.out.println("Hello Client exception: " + e);
+            System.out.println(e);
+        }
+    }
+
+}
