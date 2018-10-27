4037d33c62882afcca0714856683c24a3cac3e37
Chris McArthur <prince.chrismc@gmail.com>
2018-10-27 12:00:11 -0400

    new test for does record exist with synchronization
    some tests require the real servers to be working while other require them too be off... this is the continuation from the restart functionality ive added

--- Assignments/Records-Managment/test/Server/UdpCommunicationTest.java
+++ Assignments/Records-Managment/test/Server/UdpCommunicationTest.java
@@ -23,11 +23,16 @@
  */
 package Server;
 
+import Models.Project;
+import Models.ProjectIdentifier;
+import Models.Region;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.concurrent.locks.ReentrantLock;
 import static org.junit.Assert.assertEquals;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -36,7 +41,19 @@ import org.junit.Test;
  */
 public class UdpCommunicationTest {
 
+    final static private ReentrantLock lock = new ReentrantLock();
+    static private RegionalServer Canada;
+    static private RegionalServer UnitedStates;
+    static private RegionalServer UnitedKingdom;
+
     public UdpCommunicationTest() {
+    }
+
+    @BeforeClass
+    static public void setupRealRegionalServers() throws IOException {
+        Canada = new RegionalServer(Region.CA);
+        UnitedStates = new RegionalServer(Region.US);
+        UnitedKingdom = new RegionalServer(Region.UK);
     }
 
     @Test
@@ -48,7 +65,7 @@ public class UdpCommunicationTest {
     @Test
     public void canAnswerRecordCount() throws Exception {
         TestRegion currentRegion = TestRegion.TWO;
-        
+
         RegionalServer Server = new RegionalServer(currentRegion);
         Server.Start();
 
@@ -97,7 +114,7 @@ public class UdpCommunicationTest {
     public void canUpdateManagerId() throws Exception {
         RegionalServer Server = new RegionalServer(TestRegion.FOUR);
         Server.Start();
-        
+
         final String TEST_MANAGER_RECORD = "MR26792";
 
         DatagramSocket socket = new DatagramSocket();
@@ -120,19 +137,25 @@ public class UdpCommunicationTest {
 
     @Test
     public void doesGetRecordCountTimeout() throws Exception {
+
         RegionalServer Server = new RegionalServer(TestRegion.FIVE);
         Server.Start();
-        
+
         final String expected = TestRegion.FIVE.getPrefix() + " 0 CA TIMEOUT US TIMEOUT UK TIMEOUT";
-        final String result = Server.getRecordCount("");
-        assertEquals("Models.Regions should have TIMEOUT status", expected, result);
+        lock.lock();
+        try {
+            final String result = Server.getRecordCount("");
+            assertEquals("Models.Regions should have TIMEOUT status", expected, result);
+        } finally {
+            lock.unlock();
+        }
     }
-    
+
     @Test
     public void doesRecordNotExistInServer() throws Exception {
         RegionalServer Server = new RegionalServer(TestRegion.SIX);
         Server.Start();
-        
+
         final String TEST_MANAGER_RECORD = "MR6541";
 
         DatagramSocket socket = new DatagramSocket();
@@ -154,9 +177,48 @@ public class UdpCommunicationTest {
     }
 
     @Test
+    public void doesRecordExistInServer() throws Exception {
+        lock.lock();
+        try {
+            Canada.Start();
+            UnitedStates.Start();
+            UnitedKingdom.Start();
+
+            RegionalServer Server = new RegionalServer(TestRegion.SEVEN);
+            Server.Start();
+
+            final String TEST_MANAGER_RECORD = Server.createManagerRecord("XX0000", "John", "Smith", 25165, "john.smith@example.com",
+                    new Project(new ProjectIdentifier(0), "Huge Project", "Rich Client"), Region.CA.toString());
+
+            DatagramSocket socket = new DatagramSocket();
+            InetAddress address = InetAddress.getByName("localhost");
+            Message request = new Message(OperationCode.DOES_RECORD_EXIST, TEST_MANAGER_RECORD, address, TestRegion.SEVEN.toInt());
+
+            socket.send(request.getPacket());
+
+            byte[] buf = new byte[256];
+            DatagramPacket packet = new DatagramPacket(buf, buf.length);
+
+            socket.setSoTimeout(1000); // Set timeout in case packet is lost
+            socket.receive(packet);
+
+            Message response = new Message(packet);
+
+            assertEquals("request must be answered with an ACK", OperationCode.ACK_DOES_RECORD_EXIST, response.getOpCode());
+            assertEquals("record should not be found", "NOT FOUND", response.getData());
+
+        } finally {
+            Canada.Stop();
+            UnitedStates.Stop();
+            UnitedKingdom.Stop();
+            lock.unlock();
+        }
+    }
+
+    @Test
     public void canStopServer() throws Exception {
         TestRegion currentRegion = TestRegion.EIGTH;
-        
+
         RegionalServer Server = new RegionalServer(currentRegion);
         Server.Start();
 
@@ -176,25 +238,23 @@ public class UdpCommunicationTest {
 
         assertEquals("Get Record cound must be answered with an ACK", OperationCode.ACK_GET_RECORD_COUNT, response.getOpCode());
         assertEquals("UK RS should have Zero records", currentRegion.getPrefix() + " 0", currentRegion.getPrefix() + " " + response.getData());
-        
+
         Server.Stop();
-        
+
         socket.send(request.getPacket());
 
-        try{
-        socket.setSoTimeout(1000); // Set timeout in case packet is lost
-        socket.receive(packet);
-        }
-        catch( IOException e )
-        {
-            assertEquals( "Scoket should time out", "Receive timed out", e.getMessage());
+        try {
+            socket.setSoTimeout(1000); // Set timeout in case packet is lost
+            socket.receive(packet);
+        } catch (IOException e) {
+            assertEquals("Scoket should time out", "Receive timed out", e.getMessage());
         }
     }
 
     @Test
     public void canRestartServer() throws Exception {
         TestRegion currentRegion = TestRegion.NINE;
-        
+
         RegionalServer Server = new RegionalServer(currentRegion);
         Server.Start();
 
@@ -214,31 +274,28 @@ public class UdpCommunicationTest {
 
         assertEquals("Get Record cound must be answered with an ACK", OperationCode.ACK_GET_RECORD_COUNT, response.getOpCode());
         assertEquals("UK RS should have Zero records", currentRegion.getPrefix() + " 0", currentRegion.getPrefix() + " " + response.getData());
-        
+
         Server.Stop();
-        
+
         socket.send(request.getPacket());
 
-        try{
-        socket.setSoTimeout(1000); // Set timeout in case packet is lost
-        socket.receive(packet);
+        try {
+            socket.setSoTimeout(1000); // Set timeout in case packet is lost
+            socket.receive(packet);
+        } catch (IOException e) {
+            assertEquals("Scoket should time out", "Receive timed out", e.getMessage());
         }
-        catch( IOException e )
-        {
-            assertEquals( "Scoket should time out", "Receive timed out", e.getMessage());
-        }
-        
-        
+
         Server.Start();
-        
+
         socket.send(request.getPacket());
-        
+
         socket.receive(packet);
-        
+
         Message responseTwo = new Message(packet);
 
         assertEquals("Get Record cound must be answered with an ACK", OperationCode.ACK_GET_RECORD_COUNT, responseTwo.getOpCode());
         assertEquals("UK RS should have Zero records", currentRegion.getPrefix() + " 0", currentRegion.getPrefix() + " " + responseTwo.getData());
-        
+
     }
 }
