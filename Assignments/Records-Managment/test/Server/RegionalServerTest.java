001687c0c63c89686f2f8fc5874644c88d0b9740
Chris McArthur <prince.chrismc@gmail.com>
2018-10-27 21:48:48 -0400

    creating tests for record transfer
    returns not implemented ! TDD right here

--- Assignments/Records-Managment/test/Server/RegionalServerTest.java
+++ Assignments/Records-Managment/test/Server/RegionalServerTest.java
@@ -23,6 +23,7 @@
  */
 package Server;
 
+import Models.Feild;
 import Models.Project;
 import Models.ProjectIdentifier;
 import Models.Region;
@@ -69,4 +70,59 @@ public class RegionalServerTest {
         assertEquals("Should only be one new record", startNumberOfRecords + 1, Canada.getCurrentRecordCount());
     }
 
+    @Test
+    public void canEditMangerRecords() throws Exception {
+        int startNumberOfRecords = Canada.getCurrentRecordCount();
+        String recordId = Canada.createManagerRecord("", "john", "smith", 1001, "johm.smith@example.com", new Project(new ProjectIdentifier(0), "Huge Project", "Rich Client"), Region.CA.toString());
+        assertEquals("Should only be one new record", ++startNumberOfRecords, Canada.getCurrentRecordCount());
+
+        Canada.editRecord("", recordId, Feild.LOCATION.toString(), Region.UK.toString());
+        Canada.editRecord("", recordId, Feild.EMPLOYEE_ID.toString(), 98765);
+
+        assertEquals("Should only be same number of records", startNumberOfRecords, Canada.getCurrentRecordCount());
+    }
+
+    @Test
+    public void canEditEmployeeRecords() throws Exception {
+
+        int startNumberOfRecords = Canada.getCurrentRecordCount();
+        String recordId = Canada.createEmployeeRecord("", "james", "bond", 1001, "johm.smith@example.com", "P23001");
+        assertEquals("Should only be one new record", ++startNumberOfRecords, Canada.getCurrentRecordCount());
+
+        Canada.editRecord("", recordId, Feild.FIRST_NAME.toString(), "James");
+        Canada.editRecord("", recordId, Feild.LAST_NAME.toString(), "BOND");
+        Canada.editRecord("", recordId, Feild.EMPLOYEE_ID.toString(), 9007);
+
+        assertEquals("Should only be same number of records", startNumberOfRecords, Canada.getCurrentRecordCount());
+    }
+
+    @Test
+    public void canTransferMangerRecords() throws Exception {
+        int CanadianRecordCounter = Canada.getCurrentRecordCount();
+        int BritishRecordCounter = UnitedKingdom.getCurrentRecordCount();
+        
+        String recordId = Canada.createManagerRecord("", "john", "smith", 1001, "johm.smith@example.com", new Project(new ProjectIdentifier(0), "Huge Project", "Rich Client"), Region.CA.toString());
+        assertEquals("Should only be one new record", ++CanadianRecordCounter, Canada.getCurrentRecordCount());
+        assertEquals("Should not be a new record", BritishRecordCounter, UnitedKingdom.getCurrentRecordCount());
+
+        Canada.transferRecord("", recordId, Region.UK.toString());
+
+        assertEquals("Should one less record", --CanadianRecordCounter, Canada.getCurrentRecordCount());
+        assertEquals("Should one more new record", ++BritishRecordCounter, UnitedKingdom.getCurrentRecordCount());
+    }
+
+    @Test
+    public void canTransferEmployeeRecords() throws Exception {
+        int CanadianRecordCounter = Canada.getCurrentRecordCount();
+        int BritishRecordCounter = UnitedKingdom.getCurrentRecordCount();
+        
+        String recordId = Canada.createEmployeeRecord("", "james", "bond", 1001, "johm.smith@example.com", "P23001");
+        assertEquals("Should only be one new record", ++CanadianRecordCounter, Canada.getCurrentRecordCount());
+        assertEquals("Should not be a new record", BritishRecordCounter, UnitedKingdom.getCurrentRecordCount());
+
+        Canada.transferRecord("", recordId, Region.UK.toString());
+
+        assertEquals("Should one less record", --CanadianRecordCounter, Canada.getCurrentRecordCount());
+        assertEquals("Should one more new record", ++BritishRecordCounter, UnitedKingdom.getCurrentRecordCount());
+    }
 }
